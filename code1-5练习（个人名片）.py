f02212c2ce72cd68e64df875e548172221ba2cea
yangyousen <yangyousen1226@gmail.com>
2026-01-13 17:56:11 +0800

    Add files via upload

---
+++ code1-5练习（个人名片）.py
@@ -0,0 +1,13 @@
+name=input("请您输入您的姓名：")
+company=input("请您输入您的公司：")
+title=input("请您输入您的职位：")
+phone=input("请您输入您的电话：")
+email=input("请您输入您的邮箱：")
+print("**********************************")
+print("姓名：", name, sep="")
+print("公司：%s" % company)
+print("职位：", title, sep="")
+print("电话：%s" % phone)
+print("邮箱：", email, sep="")
+print("**********************************")
+
