0ddced96fb403266e8a204f596736d871c0f493c
yangyousen <yangyousen1226@gmail.com>
2026-01-13 18:07:41 +0800

    Delete 第2章 directory

--- 第2章/code2-10支付宝余额.py
+++
@@ -1,8 +0,0 @@
-total = 100
-print('支付宝余额: %d' % total)
-total = total + 10
-print('支付宝余额: %d' % total)
-total = total - 10
-print('支付宝余额: %.2f' % total)
-total = total - total
-print('支付宝余额: %d' % total)
